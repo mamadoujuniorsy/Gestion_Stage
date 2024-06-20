e5832f2ddf55d26154574ae6be1fc0fdea3bcdc3
Abdou Aziz Sy <abdouazizsy@esp.sn>
2024-06-20 12:29:41 +0000

    comm

---
+++ frontend/src/pages/Acceuil/Acceuil.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import acceuilNavbar from '../../components/Acceuil/AcceuilNavbar.jsx'
+function Acceuil() {
+    return (
+        <div>
+            <acceuilNavbar />
+        </div>
+    )
+}
+export default Acceuil
