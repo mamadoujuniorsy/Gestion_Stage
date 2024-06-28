edf1381c91208c15a0a422d373aca6e0d20d320d
Ousseynou <ousseynouseck@esp.sn>
2024-06-28 21:27:13 +0000

    Etudiant

---
+++ frontend/src/Etudiant/Sidebar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { RxDashboard } from "react-icons/rx";
+import { IoPerson } from "react-icons/io5";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+import logo from '../assets/logo.jpg';
+import { Link } from 'react-router-dom';
+const SideBar = () => {
+  return (
+    <div className='bg-neutral-200 w-60 p-3 flex flex-col'>
+        <img
+          src={logo}
+        />
+        <div className='flex items-center gap-2 px-3 py-2 hover:bg-blue-400'>
+        <RxDashboard />
+          <Link to='/Stage'>Postuler Stage</Link>
+        </div>
+        <div className='flex items-center gap-2 px-3 py-2 hover:bg-blue-400'>
+          <IoPerson/>
+          <Link to='/Consulter'>Consulter Stage</Link>
+        </div>
+        <div className='flex items-center gap-2 px-3 py-2 hover:bg-blue-400'>
+          <RiLogoutCircleRLine/>
+          <Link to='#' className='text-red-500'>Déconnexion</Link>
+        </div>
+      </div>      
+  )
+}
+
+export default SideBar
